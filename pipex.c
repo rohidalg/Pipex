0dd4d62f4e9da2994fca85951b41e63e47d18640
Rodrigo Hidalgo Alcantara <rodrigohidalcant@gmail.com>
2025-03-19 19:51:02 +0100

    comprobando errores

--- pipex.c
+++ pipex.c
@@ -6,17 +6,17 @@
 /*   By: rohidalg <rohidalg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 13:36:17 by rohidalg          #+#    #+#             */
-/*   Updated: 2025/03/17 17:43:37 by rohidalg         ###   ########.fr       */
+/*   Updated: 2025/03/19 19:43:22 by rohidalg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char *ft_getenv(char *name, char **env)
+char	*ft_getenv(char *name, char **env)
 {
-	int i;
-	int j;
-	char *sub;
+	int		i;
+	int		j;
+	char	*sub;
 
 	i = 0;
 	while (env[i])
@@ -37,20 +37,20 @@ char *ft_getenv(char *name, char **env)
 }
 // busca la variable del entorno que deseemmos y nos devuelve su valor
 
-char *ft_getpath(char *command, char **env)
+char	*ft_getpath(char *command, char **env)
 {
-	int i;
-	char **path;
-	char *tmp;
-	char *path_part;
-	char **cmmd;
+	int		i;
+	char	**path;
+	char	*tmp;
+	char	*path_part;
+	char	**cmmd;
 
 	i = -1;
 	path = ft_split(ft_getenv("PATH", env), ':');
 	cmmd = ft_split(command, ' ');
 	while (path[i++])
 	{
-		tmp = ft_strjoin(path[i], "/");	   // "/usr/bin/"
+		tmp = ft_strjoin(path[i], "/");       // "/usr/bin/"
 		path_part = ft_strjoin(tmp, cmmd[0]); // "/usr/bin/ls"
 		free(tmp);
 		if (access(path_part, F_OK | X_OK) == 0) // existe? | ejecutable?
@@ -65,21 +65,31 @@ char *ft_getpath(char *command, char **env)
 	return (command);
 }
 
-void ft_exit(char *str)
+void	ft_exit(char *str)
 {
 	ft_putstr_fd(str, 2);
 	exit(EXIT_FAILURE);
 }
 
-void ft_check_argv(int argc) //, char **argv)
+void	ft_check_argv(int argc, char **argv)
 {
 	if (argc != 5)
 		ft_exit("More or less than 5 arguments\n");
+	if (access(argv[1], F_OK) == -1)
+		ft_exit("No such file or directory\n");
+	if (access(argv[4], F_OK) == -1)
+		ft_exit("No such file or directory\n");
+	if (!argv[2][0] && !argv[3][0])
+        ft_exit("Permission denied\n");
+    if (!argv[3][0])
+        ft_exit("Permission denied\n");
+    if (!argv[2][0])
+        ft_exit("Permission denied\n");
 }
 
-void ft_exec(char *command, char **env)
+void	ft_exec(char *command, char **env)
 {
-	char **cmmd_part;
+	char	**cmmd_part;
 
 	cmmd_part = ft_split(command, ' ');
 	if (execve(ft_getpath(cmmd_part[0], env), cmmd_part, env) == -1)
@@ -92,9 +102,9 @@ void ft_exec(char *command, char **env)
 }
 // exec reemplaza la funcion para ejecutar el comando si llega a encontrarlo
 
-int ft_file(char *file, int option)
+int	ft_file(char *file, int option)
 {
-	int tmp;
+	int	tmp;
 
 	tmp = 0;
 	if (option == 0)
@@ -106,37 +116,40 @@ int ft_file(char *file, int option)
 	return (tmp);
 }
 
-void ft_son(char **argv, char **env,  int fd_p)
+void	ft_son(char **argv, char **env, int *fd_p)
 {
-	int fd;
+	int	fd;
 
 	fd = ft_file(argv[1], 0);
-	dup2(fd , 0); //redirige stdin al archivo
-	dup2(fd_p[1], 1); //redirige stdout al pipe de escritura
-	ft_exec(argv[2], env);
+	dup2(fd, 0);      // redirige stdin al archivo
+	dup2(fd_p[1], 1); // redirige stdout al pipe de escritura
 	close(fd);
+	ft_exec(argv[2], env);
 }
 
-void ft_father(char **argv, char **env, int fd_p)
+void	ft_father(char **argv, char **env, int *fd_p)
 {
-	int fd;
+	int	fd;
 
 	fd = ft_file(argv[4], 1);
-	dup2(fd , 0); //redirige stdout al archivo
-	dup2(fd_p, ); // redirige stdin a la lectura del pipe
+	dup2(fd, 0);      // redirige stdout al archivo
+	dup2(fd_p[0], 0); // redirige stdin a la lectura del pipe
 	close(fd);
+	ft_exec(argv[3], env);
 }
 
-int main(int argc, char **argv, char **env)
+int	main(int argc, char **argv, char **env)
 {
 	int fd_p[2];
 	pid_t pid;
-	
-	ft_check_argv(argc);
-	pid = fork();
-	if(pid == -1)
+
+	ft_check_argv(argc, argv);
+	if (pipe(fd_p) == -1)
 		exit(-1);
-	if(pid == 0)
+	pid = fork();
+	if (pid == -1)
+		exit(-1);
+	if (pid == 0)
 		ft_son(argv, env, fd_p);
 	ft_father(argv, env, fd_p);
 	return (0);
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: rohidalg <rohidalg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 10:24:05 by rohidalg          #+#    #+#             */
-/*   Updated: 2025/03/17 16:25:39 by rohidalg         ###   ########.fr       */
+/*   Updated: 2025/03/19 19:35:18 by rohidalg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@
 
 char	*ft_getenv(char *name, char **env);
 char	*ft_getpath(char *command, char **env);
-void	ft_check_argv(int argc);
+void	ft_check_argv(int argc, char **argv);
 void	ft_exit(char *str);
 void	ft_exec(char *command, char **env);
 int		ft_file(char *file, int option);
-void	ft_father(char **argv, char **env);
-void	ft_son(char **argv, char **env);
+void	ft_father(char **argv, char **env, int *fd_p);
+void	ft_son(char **argv, char **env, int *fd_p);
 
 #endif
